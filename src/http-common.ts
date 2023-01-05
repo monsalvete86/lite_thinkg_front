eb30c0e5ae4a5d9d44db9349049eb01d5065ffe4
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-05 17:01:46 -0500

    Cross cors

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://litethinking-env.eba-reisydgj.us-east-1.elasticbeanstalk.com",
   headers: {
     "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "http://litethinking-env.eba-reisydgj.us-east-1.elasticbeanstalk.com",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
   }
 });
