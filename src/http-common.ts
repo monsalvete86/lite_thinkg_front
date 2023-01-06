0533fa00297988a1842aeed6345e5ba6fcc10cdf
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-06 10:49:09 -0500

    remove allow-origin

--- src/http-common.ts
+++ src/http-common.ts
@@ -3,9 +3,5 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://litethinkingbackend-env.eba-rzcikbs3.us-east-1.elasticbeanstalk.com/",
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Content-Type": "application/json",
   }
 });
