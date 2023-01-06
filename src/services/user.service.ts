646cfcfc4cfb8365e6322698fafbdd69d79067ef
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-06 11:04:57 -0500

    Fix cors

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 import IUser from "../types/user.type"
 
-const API_URL = "https://rnbo4srhyowueqxf4iokhbxk4m0kbzay.lambda-url.us-east-1.on.aws/api/user/";
+const API_URL = "http://litethinkingbackend-env.eba-rzcikbs3.us-east-1.elasticbeanstalk.com/api/user/";
 
 export const getPublicContent = () => {
   return axios.get(API_URL + "all");
