6f509e7a25eccdd434e259e293925779a65495a5
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-06 06:41:38 -0500

    Try cross solve

--- src/http-common.ts
+++ src/http-common.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://litethinkingbackend-env.eba-rzcikbs3.us-east-1.elasticbeanstalk.com/",
   headers: {
-    "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credential": "true",
     "Access-Control-Allow-Headers": "*",
