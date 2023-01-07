eff77279418262eb6e58bc8c1ad15a067bddb530
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-06 23:54:47 -0500

    Adjust backend code

--- src/http-common.ts
+++ src/http-common.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://rnbo4srhyowueqxf4iokhbxk4m0kbzay.lambda-url.us-east-1.on.aws",
   headers: {
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Origin": "*",
+   // "Access-Control-Allow-Headers": "*",
+  //  "Access-Control-Allow-Origin": "*",
   }
 });
