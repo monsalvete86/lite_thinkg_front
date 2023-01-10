58bdefa05d428d2c7f527c3eb0539f1a22020c69
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-09 21:11:02 -0500

    test2

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://ziyxa050q7.execute-api.us-east-1.amazonaws.com/dev",
+  baseURL: process.env.PUBLIC_URL + "/dev",
   headers: {
     "Content-type": "application/json"
   }
