b13c33e63aac25e1e0bf8518e4b5a268fa96845b
cristhiammonsalvepantoja <Cristiam.Monsalve@revstarconsulting.com>
2023-07-20 13:03:58 -0500

    change server direcction

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import authHeader from "./auth-header";
 import IUser from "../types/user.type"
 
-// const API_URL = "https://w2j8ebzbl7.execute-api.us-east-1.amazonaws.com/api/user/";
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://te5s7n5qkvgqjcregadnydmzka0tzqfu.lambda-url.us-east-2.on.aws/api/user";
+// const API_URL = "http://localhost:8080/api/user";
 
 export const getPublicContent = () => {
   return axios.get(API_URL + "all");
