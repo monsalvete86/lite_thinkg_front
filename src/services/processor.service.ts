875c6575c2c55c1df92370ace8eada53a28c2aba
cristhiammonsalvepantoja <Cristiam.Monsalve@revstarconsulting.com>
2023-07-20 13:30:04 -0500

    change url

--- src/services/processor.service.ts
+++ src/services/processor.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 import ISubscription from "../types/processor.type";
 
-const API_URL = "https://te5s7n5qkvgqjcregadnydmzka0tzqfu.lambda-url.us-east-2.on.aws/api/processors";
+const API_URL = "https://v7bq77felj.execute-api.us-east-2.amazonaws.com/api/processors";
 // const API_URL = "http://localhost:8080/api/processors";
 
 export const getAll = () => {
--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 import IRole from "../types/role.type";
 
- const API_URL = "https://te5s7n5qkvgqjcregadnydmzka0tzqfu.lambda-url.us-east-2.on.aws/api/roles";
+ const API_URL = "https://v7bq77felj.execute-api.us-east-2.amazonaws.com//api/roles";
 //const API_URL = "http://localhost:8080/api/roles";
 
 export const getAll = () => {
