47cfb32ef8b750605a41d2fc009e50c3617e233d
martin-merp <marck123.mcdd@gmail.com>
2023-08-04 20:53:38 -0500

    ajustes generales

--- src/components/Reports/Reports.tsx
+++ src/components/Reports/Reports.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import ReportPDF from "../ReportPDF";
+import { PDFDownloadLink} from "@react-pdf/renderer"
 
 const Report = () => {
 
@@ -19,6 +21,37 @@ const Report = () => {
         </div>
       </div>
       <div className="list row">
+        <div className="col-md-12">
+          <label htmlFor="stateReports">Seleccionar Informe</label>
+          <div className="input-group mb-3">
+            <select name="stateReports" id="stateReports" className="custom-select">
+              <option value="">--Seleccionar -- </option>
+              <option value="SUBSCRIPTIONS">suscripciones </option>
+              <option value="CLIENTS">clientes </option>
+              <option value="PAYMENTS">pagos </option>
+              <option value="USERS">usuarios </option>
+            </select>
+            <button className="btn btn-outline-success" type="button">
+              Buscar
+            </button>
+            <PDFDownloadLink document={<ReportPDF />} fileName="report.pdf">
+                <button className="ml-2 btn btn-danger">
+                  Dowload PDF
+                </button>
+              </PDFDownloadLink>
+          </div>
+        </div>
+        {/*<div className="col-md-6">
+          <label htmlFor="stateSubscription">Estado de pagos</label>
+          <div className="input-group mb-3">
+            <select name="statePayment" id="statePayment" className="custom-select">
+              <option value="">--Seleccionar -- </option>
+              <option value="PAGADO">Pagados </option>
+              <option value="PORVENCER">Proximos a vencer </option>
+              <option value="VENCIDO">Vencida </option>
+            </select>
+          </div>
+        </div>
         <div className="col-md my-2  w-100">
           <div className="form-row w-100">
             <div className="form-group col-md-5">
@@ -38,46 +71,17 @@ const Report = () => {
               </button>
             </div>
           </div>
-        </div>
-        <div className="col-md-12">
-          <label htmlFor="stateSubscription">Estado de suscripcion</label>
-          <div className="input-group mb-3">
-            <select name="stateSubscription" id="stateSubscription" className="custom-select">
-              <option value="">--Seleccionar -- </option>
-              <option value="GENERADA">Generada </option>
-              <option value="ACEPTADA">Aceptada </option>
-              <option value="RECHAZADA">Rechazada </option>
-              <option value="CANCELADA">Cancelada </option>
-            </select>
-            <button className="btn btn-outline-success" type="button">
-              Buscar
-            </button>
-          </div>
-        </div>
-        <div className="col-md-12">
-          <label htmlFor="stateSubscription">Estado de pagos</label>
-          <div className="input-group mb-3">
-            <select name="statePayment" id="statePayment" className="custom-select">
-              <option value="">--Seleccionar -- </option>
-              <option value="PAGADO">Pagados </option>
-              <option value="PORVENCER">Proximos a vencer </option>
-              <option value="VENCIDO">Vencida </option>
-            </select>
-            <button className="btn btn-outline-success" type="button">
-              Buscar
-            </button>
-          </div>
-        </div>
+        </div>*/}
         <div className="col-md-12 list table-responsive">
           <table className="table table-striped table-bordered table-sm w-100">
             <thead>
               <tr className="text-center">
-                <th>Code</th>
-                <th>Cliente</th>
-                <th>Pago Mensual</th>
-                <th>Fecha próximo pago</th>
-                <th>Estado</th>
-                <th>Lista diaria</th>
+                <th>Example</th>
+                <th>Example</th>
+                <th>Example</th>
+                <th>Example</th>
+                <th>Example</th>
+                <th>Example</th>
                 {/*<th colSpan={2}>Acciones</th>*/}
               </tr>
             </thead>
@@ -90,10 +94,10 @@ const Report = () => {
                 <td className="text-center">example</td>
                 <td className="text-center">example</td>
                 {/*<td className="text-center">
-                  <button className="btn btn-primary">Edit</button>
+                  <button className="btn btn-primary">Example</button>
                 </td>
                 <td className="text-center">
-                  <button className="btn btn-danger">Delete</button>
+                  <button className="btn btn-danger">Example</button>
                 </td>*/}
               </tr>
             </tbody>
